3e5fe62b736c022cfe1c0261488708d5fdfc3440
Cedric Boucher <onebitalpha@gmail.com>
2025-04-16 21:23:08 -0600

    create SolverSolution class (which has SolverInputSolutions for each input of a VehicleTrip)

---
+++ src/solver_solution.py
@@ -0,0 +1,58 @@
+from solver_input_solution import SolverInputSolution, SolverFloats
+from vehicle_trip import VehicleTrip
+from datetime import datetime, timedelta
+
+AllSolverFloats = tuple[SolverFloats, ...]
+
+t0: datetime = datetime(2024, 7, 26)
+seconds_in_a_day: int = 86400
+
+class SolverSolution:
+    """
+    Represents a potential solution that the solver has generated for the entire input space,
+    which is to say, all input variables. This consists of a SolverInputSolution for each input.
+    """
+    def __init__(self, all_solution_parameters: AllSolverFloats):
+        self.__solver_input_solutions: tuple[SolverInputSolution, ...] = tuple([
+            SolverInputSolution(solution_parameters) for solution_parameters in all_solution_parameters
+        ])
+
+    @property
+    def all_solution_parameters(self) -> AllSolverFloats:
+        return tuple([sis.solution_parameters for sis in self.__solver_input_solutions])
+
+    def f(self, vehicle_trip: VehicleTrip) -> float:
+        """
+        returns calculated / estimated vehicle trip efficiency
+        based on the inputs in the provided vehicle trip and the solver solution parameters
+        """
+        y: float = (
+            self.__solver_input_solutions[0].f((vehicle_trip.date_and_time - t0).total_seconds() / seconds_in_a_day) +
+            self.__solver_input_solutions[1].f(vehicle_trip.odometer_m) +
+            self.__solver_input_solutions[2].f(vehicle_trip.trip_distance_m) +
+            self.__solver_input_solutions[3].f(vehicle_trip.vehicle_temperature_kelvin) +
+            self.__solver_input_solutions[4].f(vehicle_trip.trip_engine_running_time_s)
+        )
+
+        assert isinstance(y, float)
+        return y
+
+if __name__ == "__main__":
+    from random import random
+    test = SolverSolution((
+        tuple(random() for _ in range(39)),
+        tuple(random() for _ in range(39)),
+        tuple(random() for _ in range(39)),
+        tuple(random() for _ in range(39)),
+        tuple(random() for _ in range(39))
+    )) # type: ignore
+    vehicle_trip = VehicleTrip()
+    vehicle_trip.date_and_time = datetime(2025, 1, 1)
+    vehicle_trip.odometer_km = 123456
+    vehicle_trip.trip_distance_km = 8.5
+    vehicle_trip.vehicle_temperature_celsius = 10
+    vehicle_trip.trip_engine_running_time_m = 11.32
+    vehicle_trip.fuel_efficiency_l_per_hundred_km = 7.1
+    print(test.f(vehicle_trip))
+
+# issue: need to normalize "x" for each input to "f(x)"
