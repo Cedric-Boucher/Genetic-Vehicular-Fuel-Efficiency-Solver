fb4939cbd6f2bbfac4e52404437a385cad846f1e
Cedric Boucher <onebitalpha@gmail.com>
2025-04-30 11:44:53 -0600

    add gridlines and title to plots

--- src/display_solution.py
+++ src/display_solution.py
@@ -42,6 +42,15 @@ def plot_solution(ga_instance: pygad.GA):
     plt.legend(("Date and Time (years)", "Odometer (Gm)", "Trip Distance (100Km)", "Vehicle Temperature (K/273)", "Trip Engine Running Time (10Ks [2.78 hours])"))
     plt.ylabel("Contribution to Fuel Efficiency (Km/L)")
     plt.ylim((-50, 50))
+    plt.xticks(numpy.arange(0, 1.5, 0.2), minor = False)
+    plt.xticks(numpy.arange(0, 1.5, 0.1), minor = True)
+    plt.yticks(numpy.arange(-50, 51, 10), minor = False)
+    plt.yticks(numpy.arange(-50, 51, 5), minor = True)
+    plt.grid(True, "major", "y", linewidth = 2, alpha = 0.5)
+    plt.grid(True, "minor", "y", linewidth = 2, alpha = 0.1)
+    plt.grid(True, "major", "x", linewidth = 2, alpha = 0.5)
+    plt.grid(True, "minor", "x", linewidth = 2, alpha = 0.1)
+
 
 if __name__ == "__main__":
     if (os.path.exists(config.GA_MODEL_FILE+".pkl") and os.path.isfile(config.GA_MODEL_FILE+".pkl")):
